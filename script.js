433290c73dcaf40018384d96932994966de5f165
Tyler <twenerstrom@gmail.com>
2022-07-17 18:48:14 -0600

    final

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 var generateBtn = document.querySelector("#generate");
+var specialCharacters = "~!@#$%^&*()_+-=";
+var integers = "0123456789";
+var littleGuys =  "abcdefghhijklmnopqrstuvwxyz";
+var bigGuys = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,4 +15,72 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
+
+function generatePassword() {
+  var charAmount = prompt("How many Characters would you like to use? Must be: 12-128.");
+  var numbers = confirm("Include numbers?");
+  var lowerCase = confirm("Include LowerCase?");
+  var upperCase = confirm("Include UpperCase?");
+  var specialChar = confirm("Include Special Characters?");
+
+  var minCount = 0;
+  
+  var minNumbers = "";
+  var minLower = "";
+  var minUpper = "";
+  var minSpecial = "";
+
+  var functionArray = {
+    getnumbers: function() {
+      return integers[Math.floor(Math.random() * integers.length)];
+    },
+    getlowerCase: function() {
+      return littleGuys[Math.floor(Math.random() * littleGuys.length)];
+    },
+    getupperCase: function() {
+      return bigGuys[Math.floor(Math.random() * bigGuys.length )];
+    },
+    getspecialChar: function() {
+      return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+    }
+  };
+
+    
+    if (numbers == true) {
+      minNumbers = functionArray.getnumbers();
+      minCount++;
+    };
+      
+    if (lowerCase == true) {
+      minLower = functionArray.getlowerCase();
+      minCount++;
+    };
+    if (upperCase == true) {
+      minUpper = functionArray.getupperCase();
+      minCount++;
+    };
+    if (specialChar == true) {
+      minSpecial = functionArray.getspecialChar();
+      minCount++;
+    };
+
+    var yourSecurePassword = "";
+    for (let i = 0; i < (parseInt(charAmount) - minCount); i++) {
+      var randomNumber = Math.floor(Math.random() * 4);
+      yourSecurePassword += randomNumber;
+    }
+
+    yourSecurePassword += minNumbers;
+    yourSecurePassword += minLower;
+    yourSecurePassword += minUpper;
+    yourSecurePassword += minSpecial;
+
+    return yourSecurePassword;
+
+  }
+
+  
+
+  
+
